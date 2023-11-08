c15f35bb3fffb0160ccf16e30727f18eb0b3b3ae
E.A. Kerian Jr <kerianeminence7@gmail.com>
2023-11-08 10:44:18 -0500

    Database Inserts successful

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # KSU-Event-API
 A set of APIs to get data on Events from KSU
+
+
+Sample URL: https://se-images.campuslabs.com/clink/images/**********.jpg
+Sample GUID: https://owllife.kennesaw.edu/event/*******
--- main.py
+++ main.py
@@ -1,24 +1,62 @@
-# main.py
-import sys
-from event import Event
-from feed import get_feed
+import pymysql
+from bs4 import BeautifulSoup
+from get_event_data import events
 
-url = "https://owllife.kennesaw.edu/events.rss"
+db = pymysql.connect(host='eventtracker.ctgvnaaozzpy.us-east-2.rds.amazonaws.com',
+                     user='admin',
+                     password='Mnas1506$',
+                     db='eventtracker',
+                     port=3306)
 
-events = get_feed(url)
 
-for event in events:
-    print(f"Title: {event.title}")
-    print(f"Date: {event.pubDate}")
-    print(f"GUID: {event.guid}")
-    print(f"Description: {event.description}")
-    print(f"Category: {event.category}")
-    print(f"Location: {event.location}")
-    print(f"Host: {event.host}")
-    print(f"URL: {event.image_url}")
-    print(f"Author: {event.author}")
-    print(f"Status: {event.status}")
+def list_to_string(stringify):
+    if len(stringify) == 0:
+        return "NULL"
+    else:
+        return ", ".join(stringify)
 
-    print("--")  # separator
 
-print(sys.getsizeof(Event), "bytes") #size of an event object
+cursor = db.cursor()
+cursor.execute("DELETE FROM `eventtracker`.`eventB`")
+
+for item in [event.__dict__ for event in events]:
+    id = item['id']
+    organizationName = item['organizationName']
+    organizationNames = list_to_string(item['organizationNames'])
+    name = item['name']
+    desc = item['description']
+    # if desc is None:
+    #     desc = ""
+    description = BeautifulSoup(desc, 'html.parser').getText().rstrip().replace('\xA0', '').replace('\r\n', '').replace(
+        '\s+', '')
+    location = item['location']
+    startsOn = item['startsOn']
+    endsOn = item['endsOn']
+    theme = item['theme']
+    categoryNames = list_to_string(item['category_names'])
+    benefitNames = list_to_string(item['benefit_names'])
+    institutionId = item['institutionId']
+    organizationId = item['organizationId']
+    organizationIds = list_to_string(item['organizationIds'])
+    organizationProfilePicture = item['organizationProfilePicture']
+    branchId = item['branchId']
+    imagePath = item['imagePath']
+    visibility = item['visibility']
+    status = item['status']
+    searchScore = item['searchScore']
+
+    query = "INSERT INTO `eventtracker`.`eventB` (`id`,`organizationName`,`organizationNames`,`name`,`description`," \
+            "`location`,`startsOn`,`endsOn`,`theme`,`categoryNames`,`benefitNames`,`institutionId`,`organizationId`," \
+            "`organizationIds`,`organizationProfilePicture`,`branchId`,`imagePath`,`visibility`,`status`," \
+            "`searchScore`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) "
+
+    cursor.execute(query, (
+        id, organizationName, organizationNames, name, description, location, startsOn, endsOn, theme, categoryNames,
+        benefitNames, organizationId, institutionId, organizationIds, organizationProfilePicture, branchId, imagePath,
+        visibility, status, searchScore))
+    #print("committed")
+    db.commit()
+
+db.close()
+
+
